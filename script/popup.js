b755293aa899c93ce5ecd5124599267a03305ba0
Pavel Uzunyants <hedeeras@yandex.ru>
2022-10-27 23:47:36 +0300

    Fix critical bags

--- script/popup.js
+++ script/popup.js
@@ -1,34 +1,44 @@
 let userName = document.querySelector(".profile__user-name");
 let userDescription = document.querySelector(".profile__user-description");
+let popup = document.querySelector(".popup")
+let edditButton = document.querySelector(".profile__edit-button")
+let closeButton = document.querySelector(".popup__form-close-button")
+let userInput = document.getElementById("#popup__form-user")
+let descriptionInput = document.getElementById("#popup__form-user-description")
+let formElement = document.querySelector(".popup__form")
+let formSubmit = document.querySelector(".popup__form-submtit")
 
-let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = popup.querySelector('.popup__form-close-button');
-let popupForm = popup.querySelector('.popup__form');
-let userInput = popupForm.querySelector('.popup__form-user');
-let userInputDescription = popupForm.querySelector('.popup__form-user-description');
-let popUpSubmitButton = popupForm.querySelector('.popup__form-submtit');
-
-userInput.value = userName.textContent;
-userInputDescription.value = userDescription.textContent;
-
-function popupOpen() {
-  popupOpenButton.addEventListener('click', function() {
-    popup.classList.add('popup_opened');
-  });
-  
-  popupCloseButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
-  });
+function openPop() {
+  userInput.value = userName.textContent;
+  descriptionInput.value = userDescription.textContent;
+  popup.classList.add('popup_opened');
 }
-popupOpen();
 
-popupForm.onsubmit = function(evt) {
+function closePop() {
+  popup.classList.remove('popup_opened');
+} 
+
+edditButton.addEventListener("click", () => {
+  openPop(popup);
+});
+
+popup.addEventListener("click", (event) => {
+  let isOverlay = event.target.classList.contains("popup");
+  let isClose = event.target.classList.contains("popup__form-close-button");
+
+  if (isOverlay || isClose) {
+    closePop(popup)
+  }
+})
+
+function formSubmitHandler (evt) {
   evt.preventDefault();
-  userName.textContent = userInput.value;
-  userDescription.textContent = userInputDescription.value;
-  popUpSubmitButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
-  })
-  popupForm.addEventListener('submit', formSubmitHandler); 
+  userName.textContent = userInput.value
+  userInput.value = userName.textContent
+  userDescription.textContent = descriptionInput.value;
+  descriptionInput.value = userDescription.textContent;
+  closePop(popup)
 }
+
+formElement.addEventListener('submit', formSubmitHandler);
+
