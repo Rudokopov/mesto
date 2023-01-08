3ede94c8d7adf4b3fe8ff5bd3360b33eec670f7d
Pavel <holabeda@gmail.com>
2023-01-08 19:57:59 +0300

    feat: Проект собран на Webpack

---
+++ src/UserInfo.js
@@ -0,0 +1,22 @@
+export default class UserInfo {
+  constructor({ name, description }) {
+    this._userName = name;
+    this._userDescription = description;
+    // console.log(name);
+    // console.log(description);
+  }
+
+  getUserInfo() {
+    this._data = {
+      user: this._userName.textContent,
+      description: this._userDescription.textContent,
+    };
+    // console.log(this._data);
+    return this._data;
+  }
+
+  setUserInfo({ author, info }) {
+    this._userName.textContent = author;
+    this._userDescription.textContent = info;
+  }
+}
---
+++ src/cardsArray.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    name: 'Britan underground',
+    image:
+      'https://images.unsplash.com/photo-1546452612-548364041a03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  },
+  {
+    name: 'Uknown underground',
+    image:
+      'https://images.unsplash.com/photo-1491001224967-2c0ee18a7c42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+  {
+    name: 'Moscow underground',
+    image:
+      'https://images.unsplash.com/photo-1604585728556-1749cfd13491?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+  {
+    name: 'NewYork undeground',
+    image:
+      'https://images.unsplash.com/photo-1527546475281-b6f9a46211e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+  {
+    name: 'German undeground',
+    image:
+      'https://images.unsplash.com/photo-1530905944737-15603e6d207e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
+  },
+  {
+    name: 'Taiwan underground',
+    image:
+      'https://images.unsplash.com/photo-1510914128-918e24652488?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+];
